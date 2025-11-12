b31b8798f905c37627a009017dcd0456375c1bbd
utsab8 <utsabacharya12@gmail.com>
2025-11-12 14:27:51 +0545

    Fix: Update import case for authController

--- spendwise_backend/routes/authRoutes.js
+++ spendwise_backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authcontroller.js";
+import { registerUser, loginUser } from "../controllers/authController.js"; // ✅ Changed to authController (capital C)
 
 const router = express.Router();
 
